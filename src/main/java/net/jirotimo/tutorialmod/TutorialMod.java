e75786fc5a16cf613a0e926c53152102aea0bebc
JiroTimo <163426203+JiroTimo@users.noreply.github.com>
2024-11-29 15:55:51 +0100

    added a item

--- src/main/java/net/jirotimo/tutorialmod/TutorialMod.java
+++ src/main/java/net/jirotimo/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.jirotimo.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.jirotimo.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //comment
@@ -11,6 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
